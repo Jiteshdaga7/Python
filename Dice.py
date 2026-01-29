b30f78460fede57c2b16a752ca4bc2d8e050a0e5
Jiteshdaga7 <jiteshdaga7@gmail.com>
2026-01-29 17:03:09 +0530

    Add files via upload

---
+++ Dice.py
@@ -0,0 +1,14 @@
+import random
+random_number_1_to_6 = random.randint(1, 6)
+random_number_6_to_1 = random.randint(1, 6)
+print(random_number_1_to_6)
+print(random_number_6_to_1)
+
+temp = int(random_number_1_to_6 + random_number_6_to_1)
+
+print(temp)
+
+if temp % 2 == 0:
+    print("even")
+else:
+    print("odd")
